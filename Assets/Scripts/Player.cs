36f1719ec90bed72f0510b9db9d263eb7b7f4822
DESKTOP-4CRV8SS\KKT <celesta129@naver.com>
2021-06-06 15:35:53 +0900

    카메라 회전/ 캐릭터 이동 완료

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,35 +7,70 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;            // 5 unit per second
     public float rotationSpeed = 360f;      // 360 degree per second
 
-    CharacterController characterController;
 
-    Camera mainCam;
-    public float cameraDistance = 5f;
-    Vector3 camNormal = new Vector3(0f, 0.1f, -1f);
+    public Transform characterModel;
+    public Transform cameraArm;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-
-        mainCam = Camera.main;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        camNormal = camNormal.normalized;
-        mainCam.transform.position = transform.position + (camNormal * cameraDistance);
+        CamControl();
+        Move();
+    }
+
+    void Move()
+    {
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if(direction.sqrMagnitude > 0.01f)
+        bool isMove = direction.magnitude != 0;
+
+        Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
+        if(isMove)
         {
-            Vector3 forward = Vector3.Slerp(
-                transform.forward,
-                direction,
-                rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction)
-                );
-            transform.LookAt(transform.position + forward);
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * direction.z + lookRight * direction.x;
+
+            characterModel.forward = moveDir;
+
+            transform.position += moveDir * Time.deltaTime * moveSpeed;
         }
-        characterController.Move(direction * moveSpeed * Time.deltaTime);
+    }
+
+    void CamControl()
+    {
+        if(Input.GetMouseButton(1))     // 0 : left button , 1 : right button
+        {
+            Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            Vector3 camAngle = cameraArm.rotation.eulerAngles;
+
+            float x = camAngle.x - mouseDelta.y;
+            // cam 회전각도제한
+            if (x < 180f)
+            {
+
+                x = Mathf.Clamp(x, -1f, 70f);
+                camAngle.x = x;
+
+            }
+            else
+            {
+
+                x = Mathf.Clamp(x, 335f, 361f);
+                camAngle.x = x;
+            }
+
+            cameraArm.rotation = Quaternion.Euler(camAngle.x - mouseDelta.y,
+                                                    camAngle.y + mouseDelta.x,
+                                                    camAngle.z);
+
+        }
+
     }
 }
